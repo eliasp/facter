9bc841783d322ac6691a19fec80dea76204ee9cc
Elias Probst <mail@eliasprobst.eu>
2013-08-09 23:03:25 +0200

    Started implementing tests for reading the mac address from /sys. Not succeeding yet.

--- spec/unit/macaddress_spec.rb
+++ spec/unit/macaddress_spec.rb
@@ -16,6 +16,36 @@ describe "macaddress fact" do
   end
 
   describe "when run on Linux" do
+    describe "with /sys available" do
+      before :each do
+        Facter.fact(:kernel).stubs(:value).returns("Linux")
+        Facter.fact(:operatingsystem).stubs(:value).returns("Linux")
+        File.stubs(:exist?).with('/sys/class/net/').returns(true)
+        File.stubs(:read).with('/sys/class/net/eth0/address').returns( "00:12:3f:be:22:01\n" )
+        File.stubs(:read).with('/sys/class/net/ip6tnl0/address').returns( "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00\n" )
+        File.stubs(:read).with('/sys/class/net/lo/address').returns( "00:00:00:00:00:00\n" )
+        File.stubs(:read).with('/sys/class/net/sit0/address').returns( "00:00:00:00\n" )
+      end
+
+      it "should get netdevice directory entries in /sys/class/net/" do
+        Dir.stubs(:entries).with('/sys/class/net/').returns( ['/sys/class/net/eth0', '/sys/class/net/lo'] )
+
+        File.expects(:entries).with('/sys/class/net/').should == ['/sys/class/net/eth0', '/sys/class/net/lo']
+      end
+
+      it "should read /sys/class/net/eth0/address" do
+        Facter.fact(:macaddress).value.should == "00:12:3f:be:22:01"
+      end
+
+      describe "and only invalid interfaces available" do
+        before :each do
+          #Dir.expects(:glob).with('/sys/class/net/*').returns(['/sys/class/net/ip6tnl0', '/sys/class/net/lo', '/sys/class/net/sit0'])
+        end
+
+        it "should not return any mac address" do
+        end
+      end
+    end
     describe "without /sys available" do
       before :each do
         Facter.fact(:kernel).stubs(:value).returns("Linux")
